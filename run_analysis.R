1f1b5933d71b0619e50a11ccb1bda969b01c18ca
李扬 <ly@basecity.com>
2015-08-14 16:58:15 +0800

    update

--- run_analysis.R
+++ run_analysis.R
@@ -4,29 +4,14 @@ Run_Analysis <- function(){
     wd = getwd()
     setwd(base_folder)
     
-#     is_test_acc_x = read.table("test/Inertial Signals/body_acc_x_test.txt")
-#     is_test_acc_y = read.table("test/Inertial Signals/body_acc_y_test.txt")
-#     is_test_acc_z = read.table("test/Inertial Signals/body_acc_z_test.txt")
-#     is_test_acc = cbind(is_test_acc_x, is_test_acc_y, is_test_acc_z)
-#     
-#     is_test_gyr_x = read.table("test/Inertial Signals/body_gyro_x_test.txt")
-#     is_test_gyr_y = read.table("test/Inertial Signals/body_gyro_y_test.txt")
-#     is_test_gyr_z = read.table("test/Inertial Signals/body_gyro_z_test.txt")
-#     is_test_gry = cbind(is_test_gyr_x, is_test_gyr_y, is_test_gyr_z)
-#     
-#     #2974 obs of 768 variables
-#     is_test = cbind(is_test_acc, is_test_gry)
-    
-    
-    feature = as.vector(read.table("features.txt")[,2])
-    activity = as.vector(read.table("activity_labels.txt")[,2])
+    activity <- read.table("activity_labels.txt")[,2]
+    feature <- read.table("features.txt")[,2]
     
     #7352 obs of 561 variables
     x_train <- read.table("train/X_train.txt")
     #2974 obs of 561 variables
     x_test <- read.table("test/X_test.txt")
     x_merge <- rbind(x_train, x_test)
-    colnames(x_merge) = feature
     
     y_train <- read.table("train/Y_train.txt")
     y_test <- read.table("test/Y_test.txt")
@@ -38,22 +23,22 @@ Run_Analysis <- function(){
     
     #10299 obs of 563 variables
     #xys_merge <- cbind(x_merge, y_merge, sub_merge)
-    #step 1: Merges the training and the test sets to create one data set.
-    xys_merge <- list(x=x_merge, y=y_merge, sub=sub_merge)
+    #xys_merge <- list(x_merge, y_merge, sub_merge)
     
-    #step 2:Extracts only the measurements on the mean and standard deviation for each measurement. 
-    mean_and_std = x_merge[,feature[grepl("-mean()", feature, fixed = T) | grepl("-std()", feature, fixed = T)]]
+    colnames(x_merge) <- feature
+    step2 <- x_merge[grepl("mean()", feature, fixed = T) | grepl("std()", feature, fixed = T)]
     
-    #step 3:Uses descriptive activity names to name the activities in the data set.
-    for(i in 1:length(activity)){
-        y_merge[y_merge == i] = activity[i]
-    }
+    step3 <- factor(y_merge[,1])
+    levels(step3) <- activity
     
-    #step 4:Appropriately labels the data set with descriptive variable names. 
     colnames(y_merge) <- "activity"
+    y_merge$activity <- step3
+    
     colnames(sub_merge) <- "subject"
-    data_set = cbind(x_merge, y_merge, sub_merge)
-    #step 5:From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+    
+    xys_merge <- cbind(step2, y_merge, sub_merge)
+    
+    
     
     setwd(wd)
 }
